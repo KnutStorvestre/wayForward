761996fe18ff2cc4cbd51cbcfe11656e6b23c131
Knut Storvestre <knutmathiasstorvestre@gmail.com>
2024-04-27 14:15:07 +0200

    Landaas is now available for mobile

--- src/pages/Landaas.tsx
+++ src/pages/Landaas.tsx
@@ -3,40 +3,40 @@ import "./Landaas.css";
 
 const activityData = [
   {
-    activityTitle: "Leksehjelp",
+    activityTitle: "Leksehjelp 1",
     googleMapsLink:
       "https://www.google.com/maps/place/Universitetsaulaen/@60.3874112,5.3222966,18z/data=!4m6!3m5!1s0x463cff4f1413a72f:0x497f992ec2c19fcc!8m2!3d60.3873984!4d5.3220588!16s%2Fg%2F11flrxrvqb?entry=ttu",
     street: "museplassen 3",
     zipCode: "5007 Bergen",
-    time: "Tirsdag 15:00 - 17:00",
+    time: "Mandag 16:30 - 17:30",
     ageGroup: "1 - 4 klasse",
     description:
-      "Dette er et åpent tilbud for alle bar i Årstad bydel. Husk å ta med klær for utelek.",
+      "Ønsker du hjep med leksene dine eller hjelpe andre med lekser? Da er dette tilbudet for deg. Husk å ta med klær for utelek.",
   },
   {
-    activityTitle: "Leksehjelp",
+    activityTitle: "Leksehjelp 2",
     googleMapsLink:
       "https://www.google.com/maps/place/Universitetsaulaen/@60.3874112,5.3222966,18z/data=!4m6!3m5!1s0x463cff4f1413a72f:0x497f992ec2c19fcc!8m2!3d60.3873984!4d5.3220588!16s%2Fg%2F11flrxrvqb?entry=ttu",
     street: "museplassen 3",
     zipCode: "5007 Bergen",
-    time: "Tirsdag 15:00 - 17:00",
-    ageGroup: "1 - 4 klasse",
+    time: "Mandag 17:30 - 19:00",
+    ageGroup: "5 - 7 klasse",
     description:
-      "Dette er et åpent tilbud for alle bar i Årstad bydel. Husk å ta med klær for utelek.",
+      "Ønsker du hjep med leksene dine eller hjelpe andre med lekser? Da er dette tilbudet for deg. Husk å ta med klær for utelek.",
   },
   {
-    activityTitle: "Leksehjelp",
+    activityTitle: "Sykkelverksted",
     googleMapsLink:
       "https://www.google.com/maps/place/Universitetsaulaen/@60.3874112,5.3222966,18z/data=!4m6!3m5!1s0x463cff4f1413a72f:0x497f992ec2c19fcc!8m2!3d60.3873984!4d5.3220588!16s%2Fg%2F11flrxrvqb?entry=ttu",
     street: "museplassen 3",
     zipCode: "5007 Bergen",
-    time: "Tirsdag 15:00 - 17:00",
-    ageGroup: "1 - 4 klasse",
+    time: "Tirsdag 15:00 - 19:00",
+    ageGroup: "1 - 7 klasse",
     description:
-      "Dette er et åpent tilbud for alle bar i Årstad bydel. Husk å ta med klær for utelek.",
+      "Kan fikse alle typer sykler. Det er bare å ta med sykkelen din og komme innom.",
   },
   {
-    activityTitle: "Leksehjelp",
+    activityTitle: "Jentegruppe",
     googleMapsLink:
       "https://www.google.com/maps/place/Universitetsaulaen/@60.3874112,5.3222966,18z/data=!4m6!3m5!1s0x463cff4f1413a72f:0x497f992ec2c19fcc!8m2!3d60.3873984!4d5.3220588!16s%2Fg%2F11flrxrvqb?entry=ttu",
     street: "museplassen 3",
@@ -44,10 +44,10 @@ const activityData = [
     time: "Tirsdag 15:00 - 17:00",
     ageGroup: "1 - 4 klasse",
     description:
-      "Dette er et åpent tilbud for alle bar i Årstad bydel. Husk å ta med klær for utelek.",
+      "Nye aktiviteter hver uke. aktiviteter som gocart, sykling, klatring, og mye mer.",
   },
   {
-    activityTitle: "Leksehjelp",
+    activityTitle: "Leksehjelp 2",
     googleMapsLink:
       "https://www.google.com/maps/place/Universitetsaulaen/@60.3874112,5.3222966,18z/data=!4m6!3m5!1s0x463cff4f1413a72f:0x497f992ec2c19fcc!8m2!3d60.3873984!4d5.3220588!16s%2Fg%2F11flrxrvqb?entry=ttu",
     street: "museplassen 3",
@@ -55,7 +55,7 @@ const activityData = [
     time: "Tirsdag 15:00 - 17:00",
     ageGroup: "1 - 4 klasse",
     description:
-      "Dette er et åpent tilbud for alle bar i Årstad bydel. Husk å ta med klær for utelek.",
+      "Dette er et åpent tilbud for alle bar i Årstad bydel. Husk å ta med klær for utelek. Dette er et åpent tilbud for alle bar i Årstad bydel. Husk å ta med klær for utelek.",
   },
 ];
 
@@ -88,7 +88,8 @@ function TopText() {
     <div>
       <h1 className="extra-bold-inter">Landås aktiviteter</h1>
       <p className="semibold-inter">
-        Ting endrer seg hele tiden. Husk å sjekk{" "}
+        Disse aktivitetene er for barn og unge i Årstad bydel. Ting endrer seg
+        hele tiden. Husk å sjekk vår{" "}
         <a
           href="https://www.youtube.com/watch?v=dQw4w9WgXcQ"
           target="_blank"
