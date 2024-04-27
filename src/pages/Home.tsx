6213237b193bbbf8e9ef456442695a0c8923f491
Knut Storvestre <knutmathiasstorvestre@gmail.com>
2024-04-27 22:31:55 +0200

    Area boxes on Home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,36 @@
+import React from "react";
 import skywoods from "../assets/sky-woods.jpg";
 import "./styles/Home.css";
 
-export default function Home() {
+const Home = () => {
   return (
     <div>
-      <img className="main-img" src={skywoods} alt="Nature Climbing" />
+      <img className="main-img" src={skywoods} alt="Sky Woods" />
       <h1>Home</h1>
+      <div className="area-cards">
+        <AreaCard areaName="Landås" backgroundColor="#99C4CA" />
+        <AreaCard areaName="Sotra" backgroundColor="#FAB46E" />
+      </div>
     </div>
   );
+};
+
+interface AreaCardProps {
+  areaName: string;
+  backgroundColor: string;
 }
+
+const AreaCard: React.FC<AreaCardProps> = ({ areaName, backgroundColor }) => {
+  return (
+    <div
+      className="area-card"
+      style={{
+        backgroundColor: backgroundColor,
+      }}
+    >
+      <header>{areaName}</header>
+    </div>
+  );
+};
+
+export default Home;
