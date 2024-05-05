8b845df107d1ea8b860ce416dbb186d97ff8bd2c
Knut Storvestre <knutmathiasstorvestre@gmail.com>
2024-05-05 12:01:42 +0200

    s

--- src/content/HamburgerMenu.tsx
+++ src/content/HamburgerMenu.tsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
-import "./styles/HamburgerMenu.css";
 import { Link } from "react-router-dom";
+import "./styles/HamburgerMenu.css";
 
 const HamburgerMenu = () => {
   const [isOpen, setIsOpen] = useState(false);
