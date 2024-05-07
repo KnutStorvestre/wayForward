5d1581c6eb8a356787bfcba2b62d4935476a0d06
Knut Storvestre <knutmathiasstorvestre@gmail.com>
2024-05-07 22:18:33 +0200

    Added some text to contact page

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -1,7 +1,31 @@
+import "./styles/Contact.css";
+
 const Contact = () => {
   return (
     <div>
-      <h1>Contact Us</h1>
+      <div className="contact-text">
+        <h1>Kontakt oss</h1>
+        <div className="center-text">
+          <p>
+            Har du spørsmål eller ønsker å komme i kontakt med oss? Send oss en
+            melding på e-post eller ring oss.
+          </p>
+          <p>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Autem id
+            saepe nihil! Voluptatum magnam eligendi aut quisquam iure, ea
+            placeat eaque, aperiam optio sed non qui, nemo laborum doloribus
+            exercitationem. Lorem, ipsum dolor sit amet consectetur adipisicing
+            elit. Animi temporibus harum suscipit quis, quia dolores odit
+            dolorem architecto sint officiis fugit, qui nulla deserunt excepturi
+            amet velit repellat fugiat. Illum ea id sequi, quaerat omnis
+            repellat vitae suscipit exercitationem officiis! Quisquam, quos.
+            Quisquam, quos. Quisquam, quos. Quisquam, quos. Quisquam, quos.
+            Quisquam, quos. Quisquam, quos. Quisquam, quos. Quisquam, quos.
+            Quisquam, quos. Quisquam, quos. Quisquam, quos. Quisquam, quos.
+            Quisquam, quos.
+          </p>
+        </div>
+      </div>
     </div>
   );
 };
