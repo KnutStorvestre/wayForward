ebb2b29af1c8306b843b21b11a6dbd6a860a3871
Knut Storvestre <knutmathiasstorvestre@gmail.com>
2024-05-16 17:06:49 +0200

    Navbar now hidden when you scroll down and visible when you scroll up

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -1,10 +1,28 @@
+import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import "../styles/Navbar.css";
 import HamburgerMenu from "./HamburgerMenu";
 
 const Navbar = () => {
+  const [prevScrollPos, setPrevScrollPos] = useState(window.pageYOffset);
+  const [visible, setVisible] = useState(true);
+
+  const handleScroll = () => {
+    const currentScrollPos = window.pageYOffset;
+    const visible = prevScrollPos > currentScrollPos;
+
+    setPrevScrollPos(currentScrollPos);
+    setVisible(visible);
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, [prevScrollPos, visible]);
+
   return (
-    <nav className="nav">
+    <nav className={` ${visible ? "nav" : "hidden"}`}>
       <HamburgerMenu />
       <div className="site-title">
         <Link to="/">WayForward</Link>
