3ecaecd122f6cd0207ef365d447fb71927342107
Knut Storvestre <knutmathiasstorvestre@gmail.com>
2024-05-22 21:34:35 +0200

    back to old navbar

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -1,28 +1,10 @@
-import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import "../styles/Navbar.css";
 import HamburgerMenu from "./HamburgerMenu";
 
 const Navbar = () => {
-  const [prevScrollPos, setPrevScrollPos] = useState(window.pageYOffset);
-  const [visible, setVisible] = useState(true);
-
-  const handleScroll = () => {
-    const currentScrollPos = window.pageYOffset;
-    const visible = prevScrollPos > currentScrollPos;
-
-    setPrevScrollPos(currentScrollPos);
-    setVisible(visible);
-  };
-
-  useEffect(() => {
-    window.addEventListener("scroll", handleScroll);
-
-    return () => window.removeEventListener("scroll", handleScroll);
-  }, [prevScrollPos, visible]);
-
   return (
-    <nav className={` ${visible ? "nav" : "hidden"}`}>
+    <nav className="nav">
       <HamburgerMenu />
       <div className="site-title">
         <Link to="/">WayForward</Link>
