d78a35d7acbd3d980f4bd8147416a4c25ff30b80
Knut Storvestre <knutmathiasstorvestre@gmail.com>
2024-05-28 17:48:40 +0200

    SotraActivities is now also generic

---
+++ src/data/SotraActivitiesData.ts
@@ -0,0 +1,67 @@
+import { ActivitiesPageData } from "../types";
+
+const ActivitiesPageLandaas: ActivitiesPageData = {
+    title: "Sotra Aktiviteter",
+    topText: "Disse aktivitetene er for barn og unge på Sotra.",
+    instagramLink: "https://www.instagram.com/",
+    facebookLink: "https://www.facebook.com/",
+    activities: [
+    {
+      activityTitle: "Leksehjelp 1",
+      googleMapsLink:
+        "https://www.google.com/maps/place/Universitetsaulaen/@60.3874112,5.3222966,18z/data=!4m6!3m5!1s0x463cff4f1413a72f:0x497f992ec2c19fcc!8m2!3d60.3873984!4d5.3220588!16s%2Fg%2F11flrxrvqb?entry=ttu",
+      street: "museplassen 3",
+      zipCode: "5007 Bergen",
+      time: "Mandag 16:30 - 17:30",
+      ageGroup: "1 - 4 klasse",
+      description:
+        "Ønsker du hjep med leksene dine eller hjelpe andre med lekser? Da er dette tilbudet for deg. Husk å ta med klær for utelek.",
+    },
+    {
+      activityTitle: "Leksehjelp 2",
+      googleMapsLink:
+        "https://www.google.com/maps/place/Universitetsaulaen/@60.3874112,5.3222966,18z/data=!4m6!3m5!1s0x463cff4f1413a72f:0x497f992ec2c19fcc!8m2!3d60.3873984!4d5.3220588!16s%2Fg%2F11flrxrvqb?entry=ttu",
+      street: "museplassen 3",
+      zipCode: "5007 Bergen",
+      time: "Mandag 17:30 - 19:00",
+      ageGroup: "5 - 7 klasse",
+      description:
+        "Ønsker du hjep med leksene dine eller hjelpe andre med lekser? Da er dette tilbudet for deg. Husk å ta med klær for utelek.",
+    },
+    {
+      activityTitle: "Sykkelverksted",
+      googleMapsLink:
+        "https://www.google.com/maps/place/Universitetsaulaen/@60.3874112,5.3222966,18z/data=!4m6!3m5!1s0x463cff4f1413a72f:0x497f992ec2c19fcc!8m2!3d60.3873984!4d5.3220588!16s%2Fg%2F11flrxrvqb?entry=ttu",
+      street: "museplassen 3",
+      zipCode: "5007 Bergen",
+      time: "Tirsdag 15:00 - 19:00",
+      ageGroup: "1 - 7 klasse",
+      description:
+        "Kan fikse alle typer sykler. Det er bare å ta med sykkelen din og komme innom.",
+    },
+    {
+      activityTitle: "Jentegruppe",
+      googleMapsLink:
+        "https://www.google.com/maps/place/Universitetsaulaen/@60.3874112,5.3222966,18z/data=!4m6!3m5!1s0x463cff4f1413a72f:0x497f992ec2c19fcc!8m2!3d60.3873984!4d5.3220588!16s%2Fg%2F11flrxrvqb?entry=ttu",
+      street: "museplassen 3",
+      zipCode: "5007 Bergen",
+      time: "Tirsdag 15:00 - 17:00",
+      ageGroup: "1 - 4 klasse",
+      description:
+        "Nye aktiviteter hver uke. aktiviteter som gocart, sykling, klatring, og mye mer.",
+    },
+    {
+      activityTitle: "Leksehjelp 2",
+      googleMapsLink:
+        "https://www.google.com/maps/place/Universitetsaulaen/@60.3874112,5.3222966,18z/data=!4m6!3m5!1s0x463cff4f1413a72f:0x497f992ec2c19fcc!8m2!3d60.3873984!4d5.3220588!16s%2Fg%2F11flrxrvqb?entry=ttu",
+      street: "museplassen 3",
+      zipCode: "5007 Bergen",
+      time: "Tirsdag 15:00 - 17:00",
+      ageGroup: "1 - 4 klasse",
+      description:
+        "Dette er et åpent tilbud for alle bar i Årstad bydel. Husk å ta med klær for utelek. Dette er et åpent tilbud for alle bar i Årstad bydel. Husk å ta med klær for utelek.",
+    },
+  ],
+};
+
+export default ActivitiesPageLandaas;
