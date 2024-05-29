ac31eeb607b542c9913e22ded7ee0206447c66b0
Knut Storvestre <knutmathiasstorvestre@gmail.com>
2024-05-29 17:34:18 +0200

    The footer is now 1200px in maxwith

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -6,34 +6,36 @@ import { Link } from "react-router-dom";
 const Footer = () => {
   return (
     <footer className="footer">
-      <div className="footer-content">
-        <div className="footer-content-left">
-          <Link to="/om-oss">
-            <span>Om oss</span>
-          </Link>
-          <Link to="/partnere">
-            <span>Partnere</span>
-          </Link>
-          <a href="mailto:post@wayforward.no">post@wayforward.no</a>
-          <a href="tel:+47 55 55 55 55">+47 55 55 55 55</a>
-        </div>
-        <div className="social-media">
-          <a
-            aria-label="Gå til WayForward sin Facebook-side"
-            href="https://www.youtube.com/watch?v=dQw4w9WgXcQ"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <img src={Facebook} alt="Facebook-logo" />
-          </a>
-          <a
-            aria-label="Gå til WayForward sin Instagram-side"
-            href="https://www.youtube.com/watch?v=dQw4w9WgXcQ"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <img src={Instagram} alt="Instagram-logo" />
-          </a>
+      <div className="wide-container-footer">
+        <div className="footer-content">
+          <div className="footer-content-left">
+            <Link to="/om-oss">
+              <span>Om oss</span>
+            </Link>
+            <Link to="/partnere">
+              <span>Partnere</span>
+            </Link>
+            <a href="mailto:post@wayforward.no">post@wayforward.no</a>
+            <a href="tel:+47 55 55 55 55">+47 55 55 55 55</a>
+          </div>
+          <div className="social-media">
+            <a
+              aria-label="Gå til WayForward sin Facebook-side"
+              href="https://www.youtube.com/watch?v=dQw4w9WgXcQ"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <img src={Facebook} alt="Facebook-logo" />
+            </a>
+            <a
+              aria-label="Gå til WayForward sin Instagram-side"
+              href="https://www.youtube.com/watch?v=dQw4w9WgXcQ"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <img src={Instagram} alt="Instagram-logo" />
+            </a>
+          </div>
         </div>
       </div>
     </footer>
