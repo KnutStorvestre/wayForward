dbe8f24f7f7f7bc4a8202d4d83495e6a21c25f44
Knut Storvestre <knutmathiasstorvestre@gmail.com>
2024-05-30 14:14:07 +0200

    Added link to the acktivity cards

---
+++ src/pages/HomeworkHelp.tsx
@@ -0,0 +1,10 @@
+const HomeWorkHelp = () => {
+  return (
+    <div>
+      <h1>Homework Help</h1>
+      <p>Get help with your homework here!</p>
+    </div>
+  );
+};
+
+export default HomeWorkHelp;
