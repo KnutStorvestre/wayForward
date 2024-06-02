c94cbb60e34365bc33d9cd89951b58e18beb8be6
Knut Storvestre <knutmathiasstorvestre@gmail.com>
2024-06-02 14:38:55 +0200

    The background color of the top text now covers the entire with of the page again

--- src/pages/AboutUs.tsx
+++ src/pages/AboutUs.tsx
@@ -5,59 +5,63 @@ import styles from "./styles/AboutUs.module.css"; // Import CSS module
 
 const AboutUs = () => {
   return (
-    <div className={styles.pageContainer}>
+    <div>
       <section className={styles.topContentBackground}>
-        <div className={styles.content}>
-          <header>
-            <h1 className={styles.mainHeader}>Om oss</h1>
-          </header>
-          <article className={styles.centerText}>
-            <p className={styles.aboutUsText}>
-              Har du spørsmål eller ønsker å komme i kontakt med oss? Send oss
-              en melding på e-post eller ring oss.
-            </p>
-            <p className={styles.aboutUsText}>
-              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Autem id
-              saepe nihil! Voluptatum magnam eligendi aut quisquam iure, ea
-              placeat eaque, aperiam optio sed non qui, nemo laborum doloribus
-              exercitationem. Lorem, ipsum dolor sit amet consectetur
-              adipisicing elit. Animi temporibus harum suscipit quis, quia
-              dolores odit dolorem architecto sint officiis fugit, qui nulla
-            </p>
+        <div className={styles.pageContainer}>
+          <div className={styles.content}>
             <header>
-              <h1 className={styles.ourValuesHeader}>Våre verdier</h1>
+              <h1 className={styles.mainHeader}>Om oss</h1>
             </header>
-            <p className={styles.aboutUsText}>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis
-              inventore ut soluta ad unde odit, tempora obcaecati iure? Est,
-              qui, repellat laborum maxime excepturi optio nobis quidem fuga
-              eveniet molestiae error exercitationem vel autem nostrum debitis
-              sit et. Corrupti ex pariatur facilis eius atque excepturi sequi
-              similique provident dolor dolore!
-            </p>
-          </article>
-        </div>
-      </section>
-      <section className={styles.content}>
-        <div className={styles.groupPhoto}>
-          <img src={GroupPhoto} alt="Group of people" />
-        </div>
-        <div>
-          <header>
-            <h1 className={styles.bottomHeader}>Ansatte/Frivillige</h1>
-          </header>
-          <div className={styles.employeeCards}>
-            {employees.map((employee, index) => (
-              <EmployeeCard
-                key={index}
-                image={employee.image}
-                name={employee.name}
-                position={employee.position}
-              />
-            ))}
+            <article className={styles.centerText}>
+              <p className={styles.aboutUsText}>
+                Har du spørsmål eller ønsker å komme i kontakt med oss? Send oss
+                en melding på e-post eller ring oss.
+              </p>
+              <p className={styles.aboutUsText}>
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Autem
+                id saepe nihil! Voluptatum magnam eligendi aut quisquam iure, ea
+                placeat eaque, aperiam optio sed non qui, nemo laborum doloribus
+                exercitationem. Lorem, ipsum dolor sit amet consectetur
+                adipisicing elit. Animi temporibus harum suscipit quis, quia
+                dolores odit dolorem architecto sint officiis fugit, qui nulla
+              </p>
+              <header>
+                <h1 className={styles.ourValuesHeader}>Våre verdier</h1>
+              </header>
+              <p className={styles.aboutUsText}>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis
+                inventore ut soluta ad unde odit, tempora obcaecati iure? Est,
+                qui, repellat laborum maxime excepturi optio nobis quidem fuga
+                eveniet molestiae error exercitationem vel autem nostrum debitis
+                sit et. Corrupti ex pariatur facilis eius atque excepturi sequi
+                similique provident dolor dolore!
+              </p>
+            </article>
           </div>
         </div>
       </section>
+      <div className={styles.pageContainer}>
+        <section className={styles.content}>
+          <div className={styles.groupPhoto}>
+            <img src={GroupPhoto} alt="Group of people" />
+          </div>
+          <div>
+            <header>
+              <h1 className={styles.bottomHeader}>Ansatte/Frivillige</h1>
+            </header>
+            <div className={styles.employeeCards}>
+              {employees.map((employee, index) => (
+                <EmployeeCard
+                  key={index}
+                  image={employee.image}
+                  name={employee.name}
+                  position={employee.position}
+                />
+              ))}
+            </div>
+          </div>
+        </section>
+      </div>
     </div>
   );
 };
