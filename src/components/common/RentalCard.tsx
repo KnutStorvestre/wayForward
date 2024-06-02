5b847057eb026354a6c40e6786a7a971fd58f627
Knut Storvestre <knutmathiasstorvestre@gmail.com>
2024-06-02 13:56:33 +0200

    Created some more css modules

--- src/components/common/RentalCard.tsx
+++ src/components/common/RentalCard.tsx
@@ -1,6 +1,6 @@
-import "../styles/RentalCard.css";
 import { Link } from "react-router-dom";
 import { RentalCardData } from "../../types";
+import styles from "../styles/RentalCard.module.css"; // Import CSS module
 
 const RentalCard: React.FC<RentalCardData> = ({
   activityTitle,
@@ -12,32 +12,31 @@ const RentalCard: React.FC<RentalCardData> = ({
   linkBuilding,
 }) => {
   return (
-    <div>
-      <div className="rental-card">
+    <div className={styles.rentalCardContainer}>
+      <div className={styles.rentalCard}>
         <img src={image.url} alt={image.alt} />
-        <div className="top-bottom">
+        <div className={styles.topBottom}>
           <div>
-            {" "}
-            <div className="rental-card-text">
-              <h3 className="rental-card-title">{activityTitle}</h3>
-              <div className="flex-row">
+            <div className={styles.rentalCardText}>
+              <h3 className={styles.rentalCardTitle}>{activityTitle}</h3>{" "}
+              <div className={styles.flexRow}>
                 <a
                   href={googleMapsLink}
                   target="_blank"
                   rel="noopener noreferrer"
                 >
-                  <div className="flex-column">
-                    <span className="address">{street}</span>
-                    <span className="address">{postNumber}</span>
+                  <div className={styles.flexColumn}>
+                    <span className={styles.address}>{street}</span>{" "}
+                    <span className={styles.address}>{postNumber}</span>{" "}
                   </div>
                 </a>
               </div>
               <p>{description}</p>
             </div>
           </div>
-          <div className="bottom-info">
+          <div className={styles.bottomInfo}>
             <Link to={linkBuilding}>
-              <button className="button">Les mer</button>
+              <button className={styles.button}>Les mer</button>{" "}
             </Link>
           </div>
         </div>
