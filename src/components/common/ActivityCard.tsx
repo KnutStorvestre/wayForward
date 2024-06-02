15a0c5b09248c9856c30947ad9c7cb6e85839ad0
Knut Storvestre <knutmathiasstorvestre@gmail.com>
2024-06-02 13:43:25 +0200

    ActivityCard.tsx now uses a CSS module instead of .css

--- src/components/common/ActivityCard.tsx
+++ src/components/common/ActivityCard.tsx
@@ -1,5 +1,6 @@
-import "../styles/ActivityCard.css";
-import bergenImage from "../../assets/images/Bergen.png";
+import React from "react";
+import styles from "../styles/ActivityCard.module.css"; // Import CSS module
+import bergenImage from "../../assets/images/bergen.png";
 import calender from "../../assets/images/calender.png";
 import people from "../../assets/images/people.png";
 import { ArrowRight } from "lucide-react";
@@ -7,9 +8,6 @@ import { Link } from "react-router-dom";
 
 interface ActivityCardProps {
   activityTitle: string;
-  googleMapsLink: string;
-  street: string;
-  zipCode: string;
   time: string;
   ageGroup: string;
   description: string;
@@ -23,24 +21,35 @@ const ActivityCard: React.FC<ActivityCardProps> = ({
 }) => {
   return (
     <div>
-      <Link to="/landaas-aktiviteter/leksehjelp" className="card-activity-link">
-        <div className="card-activity">
-          <img src={bergenImage} alt="activity" className="image-activity" />
-          <div className="text-activity">
-            <p className="title-activity">{activityTitle}</p>
-            <div className="time-age-activity">
-              <div className="time">
+      <Link
+        to="/landaas-aktiviteter/leksehjelp"
+        className={styles.cardActivityLink}
+      >
+        <div className={styles.cardActivity}>
+          <img
+            src={bergenImage}
+            alt="activity"
+            className={styles.imageActivity}
+          />
+          <div className={styles.textActivity}>
+            <p className={styles.titleActivity}>{activityTitle}</p>
+            <div className={styles.timeAgeActivity}>
+              <div className={styles.time}>
                 <img src={calender} alt="calender" />
                 <p>{time}</p>
               </div>
-              <div className="age-group">
+              <div className={styles.ageGroup}>
                 <img src={people} alt="people" />
                 <p>{ageGroup}</p>
               </div>
             </div>
-            <p className="description-activity">{description}</p>
-            <div className="bottom-arrow-activity">
-              <ArrowRight className="arrow-icon" color="black" size={24} />
+            <p className={styles.descriptionActivity}>{description}</p>
+            <div className={styles.bottomArrowActivity}>
+              <ArrowRight
+                className={styles.arrowIcon}
+                color="black"
+                size={24}
+              />
             </div>
           </div>
         </div>
