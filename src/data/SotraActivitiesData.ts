11711701dd3ddf1af2eeec1aea01986c73e6a557
Knut Storvestre <knutmathiasstorvestre@gmail.com>
2024-06-05 16:10:46 +0200

    Sotra activites added

--- src/data/SotraActivitiesData.ts
+++ src/data/SotraActivitiesData.ts
@@ -1,13 +1,16 @@
 import { ActivitiesPageData } from "../types";
 
+
+
 const ActivitiesPageLandaas: ActivitiesPageData = {
-    title: "Sotra Aktiviteter",
-    topText: "Disse aktivitetene er for barn og unge på Sotra.",
+    title: "Sotra / Øygarden Aktiviteter",
+    topText: "Disse aktivitetene er for barn og unge på Sotra / Øygarden.",
     instagramLink: "https://www.instagram.com/",
     facebookLink: "https://www.facebook.com/",
     activities: [
     {
-      activityTitle: "Leksehjelp 1",
+      activityTitle: "Leksehjelp",
+      link: "/sotra-aktiviteter/leksehjelp",
       googleMapsLink:
         "https://www.google.com/maps/place/Universitetsaulaen/@60.3874112,5.3222966,18z/data=!4m6!3m5!1s0x463cff4f1413a72f:0x497f992ec2c19fcc!8m2!3d60.3873984!4d5.3220588!16s%2Fg%2F11flrxrvqb?entry=ttu",
       street: "museplassen 3",
@@ -15,7 +18,19 @@ const ActivitiesPageLandaas: ActivitiesPageData = {
       time: "Mandag 16:30 - 17:30",
       ageGroup: "1 - 4 klasse",
       description:
-        "Ønsker du hjep med leksene dine eller hjelpe andre med lekser? Da er dette tilbudet for deg. Husk å ta med klær for utelek.",
+        "Velkommen til gratis leksehjelp med servering av varm mat. Her blir det mulighet for å få hjelp med vanskelige lekser og møtes for å gjøre leksene sammen med venner.",
+    },
+    {
+      activityTitle: "Jentegruppe",
+      link: "/sotra-aktiviteter/jentegruppe",
+      googleMapsLink:
+        "https://www.google.com/maps/place/Universitetsaulaen/@60.3874112,5.3222966,18z/data=!4m6!3m5!1s0x463cff4f1413a72f:0x497f992ec2c19fcc!8m2!3d60.3873984!4d5.3220588!16s%2Fg%2F11flrxrvqb?entry=ttu",
+      street: "museplassen 3",
+      zipCode: "5007 Bergen",
+      time: "Mandag 16:30 - 17:30",
+      ageGroup: "1 - 4 klasse",
+      description:
+        "Velkommen til vår jentegruppe med gøye aktiviteter og utflukter annenhver lørdag.",
     },
   ],
 };
---
+++ src/pages/sotraAktivities/HomeworkHelpSotra.tsx
@@ -0,0 +1,64 @@
+import styles from "../styles/HomeworkHelp.module.css";
+import homeworkImg from "../../assets/images/homework-books.jpg";
+
+const HomeWorkHelpSotra = () => {
+  return (
+    <div className="page-container padding-top-7rem">
+      <div className="max-width-container">
+        <img
+          src={homeworkImg}
+          alt="Large top image"
+          className={styles.largeImage}
+        />
+        <div className="text-container horizontal-padding">
+          <p className={styles.topText}>
+            <span className={styles.mainHeader}>Leksehjelp,</span>
+            <span className={styles.location}> Sotra/Øygarden</span>
+          </p>
+          <div className={styles.bodyText}>
+            <p className={styles.strongText}>
+              Mandager fra kl. 16:00 - 19:00 vil barne- og ungdomsskoleelever få
+              mulighet til å komme for å få hjelp med lekser i lokalene til Way
+              Forward, hvor barn og ungdom fordeles etter alder og venner. Her
+              jobber ansatte hos Way Forward og frivillige fra Norsk Folkehjelp.
+            </p>
+            <p className={styles.lightText}>
+              På leksehjelpen vil det bli servert varm mat før deltakerne
+              begynner med leksene. Vi har alltid vegetarmat eller
+              vegetaralternativer. Dette ser vi på som viktig både for
+              sosialisering, inkludering og for å øke fokuset på leksene. Etter
+              alle er ferdig med ukens lekser, arrangerer vi noen felles
+              aktiviteter som bingo, Kahoot, quiz, kortspill eller annet frem
+              til aktiviteten er ferdig.
+            </p>
+            <p className={styles.lightText}>
+              Denne aktiviteten krever ikke påmelding. Det er bare til å møte
+              opp ved Grønamyrvegen 6, 5353 Straume
+            </p>
+          </div>
+        </div>
+      </div>
+      <div className={styles.wideContainerBottom}>
+        <InfoBoxHeader title="Info" />
+        Aldersgruppe eks: 1.-4. klasse og møte tidspunkt
+        <InfoBoxHeader title="Kart" />
+      </div>
+    </div>
+  );
+};
+
+interface InfoBoxHeaderProps {
+  title: string;
+}
+
+const InfoBoxHeader: React.FC<InfoBoxHeaderProps> = ({ title }) => (
+  <div>
+    <div className={styles.hrHeader}>
+      <hr className={styles.headerLine} />
+      <h1 className={styles.headerTitle}>{title}</h1>
+      <hr className={styles.headerLine} />
+    </div>
+  </div>
+);
+
+export default HomeWorkHelpSotra;
