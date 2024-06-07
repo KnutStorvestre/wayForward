57fab6d7f19d6c216a99e434fffd8e35db2aac70
Knut Storvestre <knutmathiasstorvestre@gmail.com>
2024-06-07 15:21:23 +0200

    The Infobox and the Section header is now in own tsx file

---
+++ src/pages/Util.tsx
@@ -0,0 +1,42 @@
+import styles from "./styles/ActivityPage.module.css";
+
+interface Line {
+  title: string;
+  text: string;
+}
+
+interface InfoBoxProps {
+  Lines: Line[];
+}
+
+export const InfoBox: React.FC<InfoBoxProps> = ({ Lines }) => {
+  return (
+    <div>
+      <SectionHeader title="Info" />
+      <ul className={styles.infoBoxList}>
+        {Lines.map((line, index) => (
+          <li key={index}>
+            <p>
+              <span className={styles.strongText}>{line.title}</span>
+              <span className={styles.lightText}> {line.text}</span>
+            </p>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+interface SectionHeaderProps {
+  title: string;
+}
+
+export const SectionHeader: React.FC<SectionHeaderProps> = ({ title }) => (
+  <div>
+    <div className={styles.hrHeader}>
+      <hr className={styles.headerLine} />
+      <h1 className={styles.headerTitle}>{title}</h1>
+      <hr className={styles.headerLine} />
+    </div>
+  </div>
+);
