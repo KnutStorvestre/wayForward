db40823bd5df9f588cd58d4094b664f0e212b0b7
Knut Storvestre <knutmathiasstorvestre@gmail.com>
2024-06-08 14:24:12 +0200

    All the activity cards have the same size

--- src/data/SotraActivitiesData.ts
+++ src/data/SotraActivitiesData.ts
@@ -15,7 +15,7 @@ const ActivitiesPageLandaas: ActivitiesPageData = {
         "https://www.google.com/maps/place/Universitetsaulaen/@60.3874112,5.3222966,18z/data=!4m6!3m5!1s0x463cff4f1413a72f:0x497f992ec2c19fcc!8m2!3d60.3873984!4d5.3220588!16s%2Fg%2F11flrxrvqb?entry=ttu",
       street: "museplassen 3",
       zipCode: "5007 Bergen",
-      time: "Mandag kl. 16:00 - 19:00",
+      time: "Mandag 16:00 - 19:00",
       ageGroup: "1 - 10 klasse",
       description:
         "Velkommen til gratis leksehjelp med servering av varm mat. Her blir det mulighet for å få hjelp med vanskelige lekser og møtes for å gjøre leksene sammen med venner.",
@@ -27,8 +27,8 @@ const ActivitiesPageLandaas: ActivitiesPageData = {
         "https://www.google.com/maps/place/Universitetsaulaen/@60.3874112,5.3222966,18z/data=!4m6!3m5!1s0x463cff4f1413a72f:0x497f992ec2c19fcc!8m2!3d60.3873984!4d5.3220588!16s%2Fg%2F11flrxrvqb?entry=ttu",
       street: "museplassen 3",
       zipCode: "5007 Bergen",
-      time: "Annh. lørdag kl. 12:00 - 16:00",
-      ageGroup: "13 - 20 år",
+      time: "Annh. lørdag 12:00-16:00",
+      ageGroup: "13-20 år",
       description:
         "Velkommen til vår jentegruppe med gøye aktiviteter og utflukter annenhver torsdag. Tider kan variere.",
     },
@@ -39,7 +39,7 @@ const ActivitiesPageLandaas: ActivitiesPageData = {
         "https://www.google.com/maps/place/Universitetsaulaen/@60.3874112,5.3222966,18z/data=!4m6!3m5!1s0x463cff4f1413a72f:0x497f992ec2c19fcc!8m2!3d60.3873984!4d5.3220588!16s%2Fg%2F11flrxrvqb?entry=ttu",
       street: "museplassen 3",
       zipCode: "5007 Bergen",
-      time: "XX kl. XX:XX - XX:XX",
+      time: "XX XX:XX - XX:XX",
       ageGroup: "13 - 20 år",
       description:
         "Velkommen til vår ungdomscafé på Kraftsenter for aktivitet på Sotra. Her kan ungdom  komme for å henge ut, møte venner og delta på forskjellige aktiviteter.",
