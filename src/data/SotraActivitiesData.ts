30b1d1302ed2e16684b8fc38712a4a002c26d256
Knut Storvestre <knutmathiasstorvestre@gmail.com>
2024-06-08 14:13:52 +0200

    Added some more text

--- src/data/SotraActivitiesData.ts
+++ src/data/SotraActivitiesData.ts
@@ -3,8 +3,8 @@ import { ActivitiesPageData } from "../types";
 
 
 const ActivitiesPageLandaas: ActivitiesPageData = {
-    title: "Sotra / Øygarden Aktiviteter",
-    topText: "Disse aktivitetene er for barn og unge på Sotra / Øygarden.",
+    title: "Sotra Aktiviteter",
+    topText: "Disse aktivitetene er for barn og unge på Sotra.",
     instagramLink: "https://www.instagram.com/",
     facebookLink: "https://www.facebook.com/",
     activities: [
@@ -15,8 +15,8 @@ const ActivitiesPageLandaas: ActivitiesPageData = {
         "https://www.google.com/maps/place/Universitetsaulaen/@60.3874112,5.3222966,18z/data=!4m6!3m5!1s0x463cff4f1413a72f:0x497f992ec2c19fcc!8m2!3d60.3873984!4d5.3220588!16s%2Fg%2F11flrxrvqb?entry=ttu",
       street: "museplassen 3",
       zipCode: "5007 Bergen",
-      time: "Mandag 16:30 - 17:30",
-      ageGroup: "1 - 4 klasse",
+      time: "Mandag kl. 16:00 - 19:00",
+      ageGroup: "1 - 10 klasse",
       description:
         "Velkommen til gratis leksehjelp med servering av varm mat. Her blir det mulighet for å få hjelp med vanskelige lekser og møtes for å gjøre leksene sammen med venner.",
     },
@@ -27,10 +27,22 @@ const ActivitiesPageLandaas: ActivitiesPageData = {
         "https://www.google.com/maps/place/Universitetsaulaen/@60.3874112,5.3222966,18z/data=!4m6!3m5!1s0x463cff4f1413a72f:0x497f992ec2c19fcc!8m2!3d60.3873984!4d5.3220588!16s%2Fg%2F11flrxrvqb?entry=ttu",
       street: "museplassen 3",
       zipCode: "5007 Bergen",
-      time: "Mandag 16:30 - 17:30",
-      ageGroup: "1 - 4 klasse",
+      time: "Annh. lørdag kl. 12:00 - 16:00",
+      ageGroup: "13 - 20 år",
       description:
-        "Velkommen til vår jentegruppe med gøye aktiviteter og utflukter annenhver lørdag.",
+        "Velkommen til vår jentegruppe med gøye aktiviteter og utflukter annenhver torsdag. Tider kan variere.",
+    },
+    {
+      activityTitle: "Ungdomscafè",
+      link: "/sotra-aktiviteter/Ungdomscafe",
+      googleMapsLink:
+        "https://www.google.com/maps/place/Universitetsaulaen/@60.3874112,5.3222966,18z/data=!4m6!3m5!1s0x463cff4f1413a72f:0x497f992ec2c19fcc!8m2!3d60.3873984!4d5.3220588!16s%2Fg%2F11flrxrvqb?entry=ttu",
+      street: "museplassen 3",
+      zipCode: "5007 Bergen",
+      time: "XX kl. XX:XX - XX:XX",
+      ageGroup: "13 - 20 år",
+      description:
+        "Velkommen til vår ungdomscafé på Kraftsenter for aktivitet på Sotra. Her kan ungdom  komme for å henge ut, møte venner og delta på forskjellige aktiviteter.",
     },
   ],
 };
