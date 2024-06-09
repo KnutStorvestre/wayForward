5e12245bef3e28e85678e9613fe919a943f7ad73
Knut Storvestre <knutmathiasstorvestre@gmail.com>
2024-06-09 16:25:59 +0200

    The Sotra acivities code is now DRY

--- src/pages/sotraAktivities/HomeworkHelpSotra.tsx
+++ src/pages/sotraAktivities/HomeworkHelpSotra.tsx
@@ -1,55 +1,35 @@
-import styles from "../styles/ActivityPage.module.css";
 import homeworkImg from "../../assets/images/homework-books.jpg";
-import { SectionHeader, InfoBox } from "../Util";
+import ActivityPage from "../ActivityPage";
 
 const HomeWorkHelpSotra = () => {
+  const bodyText = [
+    {
+      isStrong: true,
+      content:
+        "Mandager fra kl. 16:00 - 19:00 vil barne- og ungdomsskoleelever få mulighet til å komme for å få hjelp med lekser i lokalene til Way Forward, hvor barn og ungdom fordeles etter alder og venner. Her jobber ansatte hos Way Forward og frivillige fra Norsk Folkehjelp.",
+    },
+    {
+      isStrong: false,
+      content: `På leksehjelpen vil det bli servert varm mat før deltakerne begynner med leksene. Vi har alltid vegetarmat eller vegetaralternativer. Dette ser vi på som viktig både for sosialisering, inkludering og for å øke fokuset på leksene. Etter alle er ferdig med ukens lekser, arrangerer vi noen felles aktiviteter som bingo, Kahoot, quiz, kortspill eller annet frem til aktiviteten er ferdig.`,
+    },
+  ];
+
+  const infoLines = [
+    { title: "Tidspunkt:", text: "Mandag kl. 16 - 19" },
+    { title: "Aldersgruppe:", text: "1 - 10 klasse" },
+    { title: "Påmelding:", text: "Det er bare å møte opp." },
+    { title: "Adresse:", text: "Grønamyrvegen 6, 5353 Straume" },
+  ];
+
   return (
-    <div className="page-container padding-top-7rem">
-      <div className="max-width-container">
-        <img
-          src={homeworkImg}
-          alt="Large top image"
-          className={styles.largeImage}
-        />
-        <div className="text-container horizontal-padding">
-          <p className={styles.topText}>
-            <span className={styles.mainHeader}>Leksehjelp,</span>
-            <span className={styles.location}> Sotra</span>
-          </p>
-          <div className={styles.bodyText}>
-            <p className={styles.strongTextParagraph}>
-              Mandager fra kl. 16:00 - 19:00 vil barne- og ungdomsskoleelever få
-              mulighet til å komme for å få hjelp med lekser i lokalene til Way
-              Forward, hvor barn og ungdom fordeles etter alder og venner. Her
-              jobber ansatte hos Way Forward og frivillige fra Norsk Folkehjelp.
-            </p>
-            <p className={styles.lightTextParagraph}>
-              På leksehjelpen vil det bli servert varm mat før deltakerne
-              begynner med leksene. Vi har alltid vegetarmat eller
-              vegetaralternativer. Dette ser vi på som viktig både for
-              sosialisering, inkludering og for å øke fokuset på leksene. Etter
-              alle er ferdig med ukens lekser, arrangerer vi noen felles
-              aktiviteter som bingo, Kahoot, quiz, kortspill eller annet frem
-              til aktiviteten er ferdig.
-            </p>
-          </div>
-          <InfoBox
-            Lines={[
-              { title: "Tidspunkt:", text: "Mandag kl. 16 - 19" },
-              { title: "Aldersgruppe:", text: "1 - 10 klasse" },
-              {
-                title: "Påmelding:",
-                text: `Det er bare å møte opp.`,
-              },
-              { title: "Adresse:", text: "Grønamyrvegen 6, 5353 Straume" },
-            ]}
-          />
-        </div>
-      </div>
-      <div className={styles.wideContainerBottom}>
-        <SectionHeader title="Kart" />
-      </div>
-    </div>
+    <ActivityPage
+      imgSrc={homeworkImg}
+      imgAlt="Large top image"
+      header="Leksehjelp"
+      location="Sotra"
+      textParagraphs={bodyText}
+      infoLines={infoLines}
+    />
   );
 };
 
--- src/pages/sotraAktivities/YouthCafe.tsx
+++ src/pages/sotraAktivities/YouthCafe.tsx
@@ -1,58 +1,42 @@
-import styles from "../styles/ActivityPage.module.css";
 import homeworkImg from "../../assets/images/homework-books.jpg";
-import { SectionHeader, InfoBox } from "../Util";
+import ActivityPage from "../ActivityPage";
 
 const YouthCafe = () => {
+  const textParagraphs = [
+    {
+      isStrong: true,
+      content:
+        "Velkommen til vår ungdomscafé på Kraftsenter for aktivitet. Her kan ungdom fra 13 og oppover komme for å henge ut, møte venner og delta på forskjellige aktiviteter på huset. Det blir servert varm mat hver gang.",
+    },
+    {
+      isStrong: false,
+      content:
+        "Vi har aktiviteter som Playstation (Fifa), tegning, bordtennis, foosball turnering, Kahoot, Bingo, Uno og mye mer. Om været er fint åpner vi opp for uteaktiviteter som fotballturneringer på Sotra arena eller liknende.",
+    },
+    {
+      isStrong: false,
+      content:
+        "Innimellom drar ungdomscaféen på utflukt til steder som Rush, Gokart Sartor, Megazone, Fangene på fortet, skøyter, badeland, bowling og mye mer.",
+    },
+  ];
+
+  const infoLines = [
+    { title: "Tidspunkt:", text: "XXXXX" },
+    { title: "Aldersgruppe:", text: "13 - 20 år" },
+    { title: "Pris:", text: "Gratis" },
+    { title: "Påmelding:", text: "Det er bare å møte opp." },
+    { title: "Adresse:", text: "XXXX" },
+  ];
+
   return (
-    <div className="page-container padding-top-7rem">
-      <div className="max-width-container">
-        <img
-          src={homeworkImg}
-          alt="Large top image"
-          className={styles.largeImage}
-        />
-        <div className="text-container horizontal-padding">
-          <p className={styles.topText}>
-            <span className={styles.mainHeader}>Ungdomscafé,</span>
-            <span className={styles.location}> Sotra</span>
-          </p>
-          <div className={styles.bodyText}>
-            <p className={styles.strongTextParagraph}>
-              Velkommen til vår ungdomscafé på Kraftsenter for aktivitet. Her
-              kan ungdom fra 13 og oppover komme for å henge ut, møte venner og
-              delta på forskjellige aktiviteter på huset. Det blir servert varm
-              mat hver gang.
-            </p>
-            <p className={styles.lightTextParagraph}>
-              Vi har aktiviteter som Playstation (Fifa), tegning, bordtennis,
-              foosball turnering, Kahoot, Bingo, Uno og mye mer. Om været er
-              fint åpner vi opp for uteaktiviteter som fotballturneringer på
-              Sotra arena eller liknende.
-            </p>
-            <p className={styles.lightTextParagraph}>
-              Innimellom drar ungdomscaféen på utflukt til steder som Rush,
-              Gokart Sartor, Megazone, Fangene på fortet, skøyter, badeland,
-              bowling og mye mer.
-            </p>
-          </div>
-          <InfoBox
-            Lines={[
-              { title: "Tidspunkt:", text: "XXXXX" },
-              { title: "Aldersgruppe:", text: "13 - 20 år" },
-              { title: "Pris:", text: "Gratis" },
-              {
-                title: "Påmelding:",
-                text: `Det er bare å møte opp.`,
-              },
-              { title: "Adresse:", text: "XXXX" },
-            ]}
-          />
-        </div>
-      </div>
-      <div className={styles.wideContainerBottom}>
-        <SectionHeader title="Kart" />
-      </div>
-    </div>
+    <ActivityPage
+      imgSrc={homeworkImg}
+      imgAlt="Large top image"
+      header="Ungdomscafé"
+      location="Sotra"
+      textParagraphs={textParagraphs}
+      infoLines={infoLines}
+    />
   );
 };
 
