dba5f9e4191718382ad87ae1dcbfac4d0e473a32
Knut Storvestre <knutmathiasstorvestre@gmail.com>
2024-06-09 16:25:59 +0200

    The Sotra acivities code is now DRY

---
+++ src/pages/ActivityPage.tsx
@@ -0,0 +1,54 @@
+import styles from "./styles/ActivityPage.module.css";
+import { SectionHeader, InfoBox } from "./Util";
+
+interface ActivityPageProps {
+  imgSrc: string;
+  imgAlt: string;
+  header: string;
+  location: string;
+  textParagraphs: { content: string; isStrong: boolean }[];
+  infoLines: { title: string; text: string }[];
+}
+
+const ActivityPage: React.FC<ActivityPageProps> = ({
+  imgSrc,
+  imgAlt,
+  header,
+  location,
+  textParagraphs,
+  infoLines,
+}) => {
+  return (
+    <div className="page-container padding-top-7rem">
+      <div className="max-width-container">
+        <img src={imgSrc} alt={imgAlt} className={styles.largeImage} />
+        <div className="text-container horizontal-padding">
+          <p className={styles.topText}>
+            <span className={styles.mainHeader}>{header},</span>
+            <span className={styles.location}> {location}</span>
+          </p>
+          <div className={styles.bodyText}>
+            {textParagraphs.map((text, index) => (
+              <p
+                key={index}
+                className={
+                  text.isStrong
+                    ? styles.strongTextParagraph
+                    : styles.lightTextParagraph
+                }
+              >
+                {text.content}
+              </p>
+            ))}
+          </div>
+          <InfoBox Lines={infoLines} />
+        </div>
+      </div>
+      <div className={styles.wideContainerBottom}>
+        <SectionHeader title="Kart" />
+      </div>
+    </div>
+  );
+};
+
+export default ActivityPage;
