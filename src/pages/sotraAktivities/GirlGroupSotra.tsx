2a6d52e515204419fa9306699b7f437244dc44c5
Knut Storvestre <knutmathiasstorvestre@gmail.com>
2024-06-09 18:18:50 +0200

    Made the length of some functions shorter

--- src/pages/sotraAktivities/GirlGroupSotra.tsx
+++ src/pages/sotraAktivities/GirlGroupSotra.tsx
@@ -1,51 +1,49 @@
 import homeworkImg from "../../assets/images/homework-books.jpg";
 import ActivityPage from "../ActivityPage";
 
-const GirlGroupSotra = () => {
-  const textParagraphs = [
-    {
-      isStrong: true,
-      content: `
+const textParagraphs = [
+  {
+    isStrong: true,
+    content: `
       Med base på Kraftsenter for aktivitet på Sotra, arrangerer vi jentegruppe for jenter som vil delta på aktiviteter sammen.
       Her er det fullt mulig å ta med venner eller komme alene.
       Det er to kvinnelige aktivitetsledere som jobber på denne aktiviteten, og varm mat serveres som oftest.
       `,
-    },
-    {
-      isStrong: false,
-      content: `
+  },
+  {
+    isStrong: false,
+    content: `
       Aktiviteter på huset varierer fra gang til gang, fra tegning, maling, baking, kakesalg, filmvisning, quiz og mye mer!
       Minst annenhver gang drar vi på utflukt til steder som badeland, Gokart, klatreparken Høyt og Lavt, skøyter, kino, restaurantbesøk, tivoli, bowling, “Fangene på fortet” og mye mye mer!
       `,
-    },
-    {
-      isStrong: false,
-      content: `
+  },
+  {
+    isStrong: false,
+    content: `
       Vi ønsker å ha Øygardens beste jentegruppe, og tar gjerne forslag fra alle deltakere om hva vi kan gjøre til neste gang. <3
       `,
-    },
-  ];
+  },
+];
 
-  const infoLines = [
-    { title: "Tidspunkt:", text: "Annenhver lørdag kl. 12:00 - 16:00" },
-    { title: "Aldersgruppe:", text: "13 - 20 år" },
-    {
-      title: "Påmelding:",
-      text: "Kontakt Rasmus på 40 40 60 98 for info om påmelding og datoer for jentegruppen.",
-    },
-    { title: "Adresse:", text: "XXXX" },
-  ];
+const infoLines = [
+  { title: "Tidspunkt:", text: "Annenhver lørdag kl. 12:00 - 16:00" },
+  { title: "Aldersgruppe:", text: "13 - 20 år" },
+  {
+    title: "Påmelding:",
+    text: "Kontakt Rasmus på 40 40 60 98 for info om påmelding og datoer for jentegruppen.",
+  },
+  { title: "Adresse:", text: "XXXX" },
+];
 
-  return (
-    <ActivityPage
-      imgSrc={homeworkImg}
-      imgAlt="Large top image"
-      header="Jentegruppe"
-      location="Sotra"
-      textParagraphs={textParagraphs}
-      infoLines={infoLines}
-    />
-  );
+const GirlGroupSotra = () => {
+  <ActivityPage
+    imgSrc={homeworkImg}
+    imgAlt="Large top image"
+    header="Jentegruppe"
+    location="Sotra"
+    textParagraphs={textParagraphs}
+    infoLines={infoLines}
+  />;
 };
 
 export default GirlGroupSotra;
