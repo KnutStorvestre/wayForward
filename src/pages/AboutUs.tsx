413127bc1c15937578b26c57096f4682e49e62a6
Knut Storvestre <knutmathiasstorvestre@gmail.com>
2024-06-23 01:16:25 +0200

    The text styling is better

--- src/pages/AboutUs.tsx
+++ src/pages/AboutUs.tsx
@@ -13,38 +13,41 @@ const AboutUs = () => {
       >
         <div>
           <div className="text-container">
-            <header>
-              <h1 className={styles.mainHeader}>Om oss</h1>
-            </header>
-            <article className={styles.centerText}>
-              <p>
-                Way Forward er en frivillig organisasjon som hovedsakelig jobber
-                med å integrere og inkludere flerkulturelle barn og ungdom inn i
-                det norske samfunnet med et ekstra fokus på at jenter skal delta
-                i like stor grad som guttene. Organisasjonen ble startet av
-                Richard Kiwanuka i 2004 og bruker nå leksehjelp, sykkelverksted,
-                musikkstudio, jentegruppe og andre fritidsaktiviteter for å nå
-                sine mål. Way Forward har et høyt fokus på å bygge opp et
-                samfunn hvor alle har muligheten til å delta i sosiale
-                aktiviteter uansett bakgrunn og økonomisk situasjon.
-              </p>
-              <p>
-                I tillegg til å arrangere egne aktiviteter hjelper også Way
-                Forward barn og unge inn i eksisterende idrettstilbud gjennom å
-                kommunisere med idrettsklubber og hjelpe til økonomisk med
-                kontingenter og innkjøp av idrettsutstyr. Tidligere har også
-                organisasjonen arrangert dansekurs og svømmekurs. Gjennom årene
-                har over 1200 barn og ungdom lært å svømme gjennom kursene.
-              </p>
-              <p>
-                Richard Kiwanuka, leder av Way Forward, har mottatt flere priser
-                gjennom årene. Prisene han har mottatt er Ildsjelprisen,
-                Omsorgsprisen, Årets forbilde, Topp 10 Internasjonal leder,
-                Likestillingsprisen og Barn av Jorden. I 2020 mottok Way Forward
-                Vestlands Barrikadepris fra Fri Vestland for arrangeringen av
-                Black Lives Matter demonstrasjonen i Bergen.
-              </p>
-            </article>
+            <h1 className={styles.mainHeader}>Om oss</h1>
+            <p className={styles.mainParagraph}>
+              Way Forward er en frivillig organisasjon som hovedsakelig jobber
+              med å integrere og inkludere flerkulturelle barn og ungdom inn i
+              det norske samfunnet med et ekstra fokus på at jenter skal delta i
+              like stor grad som guttene. Organisasjonen ble startet av Richard
+              Kiwanuka i 2004 og bruker nå leksehjelp, sykkelverksted,
+              musikkstudio, jentegruppe og andre fritidsaktiviteter for å nå
+              sine mål. Way Forward har et høyt fokus på å bygge opp et samfunn
+              hvor alle har muligheten til å delta i sosiale aktiviteter uansett
+              bakgrunn og økonomisk situasjon.
+            </p>
+            <p className={styles.mainParagraph}>
+              I tillegg til å arrangere egne aktiviteter hjelper også Way
+              Forward barn og unge inn i eksisterende idrettstilbud gjennom å
+              kommunisere med idrettsklubber og hjelpe til økonomisk med
+              kontingenter og innkjøp av idrettsutstyr. Tidligere har også
+              organisasjonen arrangert dansekurs og svømmekurs. Gjennom årene
+              har over 1200 barn og ungdom lært å svømme gjennom kursene.
+            </p>
+            <p className={styles.mainParagraph}>
+              Richard Kiwanuka, leder av Way Forward, har mottatt flere priser
+              gjennom årene. Prisene han har mottatt er{" "}
+              <span className={styles.mainEmphasis}>
+                Ildsjelprisen, Omsorgsprisen, Årets forbilde, Topp 10
+                Internasjonal leder, Likestillingsprisen
+              </span>{" "}
+              og <span className={styles.mainEmphasis}>Barn av Jorden</span>. I
+              2020 mottok Way Forward{" "}
+              <span className={styles.mainEmphasis}>
+                Vestlands Barrikadepris
+              </span>{" "}
+              fra Fri Vestland for arrangeringen av Black Lives Matter
+              demonstrasjonen i Bergen.
+            </p>
           </div>
         </div>
       </section>
