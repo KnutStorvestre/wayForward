e11f1f7852b0901f84566b7b2e2128b6a28ad897
Knut Storvestre <knutmathiasstorvestre@gmail.com>
2024-06-22 14:53:51 +0200

    Sponsors page is better

--- src/pages/Partners.tsx
+++ src/pages/Partners.tsx
@@ -1,30 +1,40 @@
 import "./styles/Partners.css";
-import BergenKommuneLogo from "../assets/sponsors/bergen-kommune-vertikal-logo.png";
-import BufdirLogo from "../assets/sponsors/bufdir-logo.png";
-import EquinorLogo from "../assets/sponsors/equinor-logo.png";
-import FanaSparebank from "../assets/sponsors/fana-sparebank-logo.webp";
-import NavLogo from "../assets/sponsors/nav-logo.png";
-import NorskFolkeHjelpLogo from "../assets/sponsors/norsk-folkehjelp-logo.png";
-import OygardenLogo from "../assets/sponsors/oygarden-logo.png";
-import SparebankenVest from "../assets/sponsors/sparebanken-vest-logo.png";
-import StiftelsenDam from "../assets/sponsors/stiftelsen-dam-logo.png";
-import UlvsnesoySkolehjem from "../assets/sponsors/ulvsnesoy-skolehjem-logo.jpeg";
+import BergenKommune from "../assets/newLogoes/Bergen-kommune.png";
+import Bufdir from "../assets/newLogoes/bufdir-logo.png";
+import equinor from "../assets/newLogoes/equinor-logo.png";
+import spv from "../assets/newLogoes/SPV.png";
+
+import damLogo from "../assets/newLogoes/dam-logo.png";
+import norskFolkehjelp from "../assets/newLogoes/norsk-folkehjelp-logo.png";
+import navLogo from "../assets/newLogoes/nav-logo.png";
+import ulvsenoyLogo from "../assets/newLogoes/ulvsnesoy-skolehjem-logo.png";
+
+import sparebank1 from "../assets/newLogoes/sparebank1.png";
+import oygarden from "../assets/newLogoes/oygarden-logo.png";
+import fanaBank from "../assets/newLogoes/fana-bank.png";
 
 const Partners = () => {
   return (
     <div className="page-container">
       <h1 style={{ padding: "6rem 0 2rem 2rem" }}>Partnere</h1>
       <div className="partners">
-        <img src={BergenKommuneLogo} alt="Bergen Kommune" />
-        <img src={BufdirLogo} alt="Bufdir" />
-        <img src={EquinorLogo} alt="Equinor" />
-        <img src={FanaSparebank} alt="Fana Sparebank" />
-        <img src={NavLogo} alt="NAV" />
-        <img src={NorskFolkeHjelpLogo} alt="Norsk Folkehjelp" />
-        <img src={OygardenLogo} alt="Øygarden Kommune" />
-        <img src={SparebankenVest} alt="Sparebanken Vest" />
-        <img src={StiftelsenDam} alt="Stiftelsen Dam" />
-        <img src={UlvsnesoySkolehjem} alt="Ulvsnesøy Skolehjem" />
+        <div className="partners-row">
+          <img src={BergenKommune} alt="Bergen Kommune" />
+          <img src={Bufdir} alt="Bergen Kommune" />
+          <img src={equinor} alt="Bergen Kommune" />
+          <img src={spv} alt="Bergen Kommune" />
+        </div>
+        <div className="partners-row">
+          <img src={damLogo} alt="Bergen Kommune" />
+          <img src={norskFolkehjelp} alt="Bergen Kommune" />
+          <img src={navLogo} alt="Bergen Kommune" />
+          <img src={ulvsenoyLogo} alt="Bergen Kommune" />
+        </div>
+        <div className="partners-row-bottom">
+          <img src={sparebank1} alt="Bergen Kommune" />
+          <img src={oygarden} alt="Bergen Kommune" />
+          <img src={fanaBank} alt="Bergen Kommune" />
+        </div>
       </div>
     </div>
   );
