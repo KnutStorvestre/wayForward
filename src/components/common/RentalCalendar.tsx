7033840767245b5d90d9a8b7056c239644e1e620
Knut Storvestre <knutmathiasstorvestre@gmail.com>
2024-07-03 21:38:46 +0200

    chore: Refactor RentalCalendar component and update styles

--- src/components/common/RentalCalendar.tsx
+++ src/components/common/RentalCalendar.tsx
@@ -2,21 +2,40 @@ import { useState } from "react";
 import { DayPicker } from "react-day-picker";
 import { nb } from "date-fns/locale";
 import "react-day-picker/dist/style.css";
-import styles from "../styles/customCalendar.module.css";
+import styles from "../styles/CustomCalendar.module.css";
+
+const today = new Date();
+
+const dateSixMonths = new Date();
+dateSixMonths.setMonth(today.getMonth() + 6);
 
 const modifiers = {
-  highlighted: new Date(2024, 6, 4), // July 4th, 2024
-  selected: new Date(2024, 6, 5), // July 5th, 2024
+  greenDates: { after: today.getDate() + 1, before: dateSixMonths },
+  redDates: [
+    new Date(2024, 6, 4), // July 4th, 2024
+    new Date(2024, 6, 5), // July 5th, 2024
+    new Date(2024, 6, 6), // July 6th, 2024
+    new Date(2024, 6, 7), // July 7th, 2024
+  ],
+  previousDays: {
+    before: today,
+  },
 };
 
+const hiddenDates = [{ after: dateSixMonths }];
+
 const modifiersStyles = {
-  highlighted: {
-    backgroundColor: "#a6e4ff",
+  redDates: {
+    backgroundColor: "#ffcccc", // Light red
     color: "#000000",
   },
-  selected: {
-    backgroundColor: "#7accc8",
+  greenDates: {
+    backgroundColor: "#ccffcc", // Light green
     color: "#000000",
+  },
+  previousDays: {
+    backgroundColor: "#eeeeee",
+    color: "#666666",
   },
 };
 
@@ -24,21 +43,27 @@ const RentalCalendar = ({}) => {
   const [selectedDay, setSelectedDay] = useState<Date | undefined>(undefined);
 
   return (
-    <DayPicker
-      className={styles.DayPicker}
-      selected={selectedDay}
-      onSelect={setSelectedDay}
-      locale={nb}
-      modifiers={modifiers}
-      modifiersStyles={modifiersStyles}
-    />
+    <div className={styles.descriptionCalendarContainer}>
+      <div className={styles.descriptionContainer}>
+        <p>
+          Velg en dato for å se tilgjengelige tider. Røde datoer er ikke
+          tilgjengelige.
+        </p>
+      </div>
+      <div className={styles.dayPickerContainer}>
+        <DayPicker
+          fromDate={today}
+          toDate={dateSixMonths}
+          selected={selectedDay}
+          onSelect={setSelectedDay}
+          locale={nb}
+          modifiers={modifiers}
+          modifiersStyles={modifiersStyles}
+          hidden={hiddenDates}
+        />
+      </div>
+    </div>
   );
 };
 
 export default RentalCalendar;
-
-/*
-
-
-
- */
