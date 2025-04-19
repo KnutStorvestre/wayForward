5063b39fc0a5cc0bda779cf14a1137df9e4dd7c7
Knut Storvestre <knutmathiasstorvestre@gmail.com>
2025-04-19 17:41:42 +0200

    used npm npm audit fix to remove vunrabilities.

--- vite.config.ts
+++ vite.config.ts
@@ -2,14 +2,10 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import dotenv from 'dotenv';
 
-// Load the environment variables from the corresponding .env file
 export default defineConfig(({ mode }) => {
   dotenv.config({ path: `.env.${mode}` });
 
   return {
     plugins: [react()],
-    define: {
-      'process.env': process.env,
-    },
   };
 });
